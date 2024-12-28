aa478c41190e3c899134270ed17eef53fb01945a
Zulfugar Abdullayev <zulfizupy@gmail.com>
2024-12-28 15:06:40 +0200

    customer related command namins and type names changed

--- customerCommand.go
+++ customerCommand.go
@@ -9,35 +9,35 @@ import (
 )
 
 type CustomerCmdFlags struct {
-	Add           string
-	AddVehicle    string
-	Edit          string
-	Delete        int64
-	DeleteVehicle string
-	List          bool
+	AddCustomer               string
+	AddVehicleToCustomer      string
+	EditCustomer              string
+	DeleteCustomer            int64
+	DeleteVehicleFromCustomer string
+	ListCustomers              bool
 }
 
 func CustomersNewCmdFlags() *CustomerCmdFlags {
 	cf := CustomerCmdFlags{}
 
-	flag.StringVar(&cf.Add, "add", "", "Add a new customer using the following format --> firstName:lastName:phoneNumber:email:personalID")
-	flag.StringVar(&cf.AddVehicle, "addVehicle", "", "Add an existing vehicle to a customer using the following format --> personalID:plateNumber")
-	flag.StringVar(&cf.Edit, "edit", "", "Edit a customer's contact data using their personal ID using the following format --> personalID:phoneNumber:email")
-	flag.Int64Var(&cf.Delete, "delete", 0, "Delete a customer using their personal ID using the following format --> personalID")
-	flag.StringVar(&cf.DeleteVehicle, "deleteVehicle", "", "Delete a vehicle from customer's rented cars")
-	flag.BoolVar(&cf.List, "list", false, "List all customers")
+	flag.StringVar(&cf.AddCustomer, "addCustomer", "", "Add a new customer using the following format --> firstName:lastName:phoneNumber:email:personalID")
+	flag.StringVar(&cf.AddVehicleToCustomer, "addVehicleToCustomer", "", "Add an existing vehicle to a customer using the following format --> personalID:plateNumber")
+	flag.StringVar(&cf.EditCustomer, "editCustomer", "", "Edit a customer's contact data using their personal ID using the following format --> personalID:phoneNumber:email")
+	flag.Int64Var(&cf.DeleteCustomer, "deleteCustomer", 0, "Delete a customer using their personal ID using the following format --> personalID")
+	flag.StringVar(&cf.DeleteVehicleFromCustomer, "deleteVehicleFromCustomer", "", "Delete a vehicle from customer's rented cars")
+	flag.BoolVar(&cf.ListCustomers, "listCustomers", false, "List all customers")
 
-	flag.Parse()
+	// flag.Parse()
 
 	return &cf
 }
 
 func (cf *CustomerCmdFlags) Execute(customers *Customers, vehicles Vehicles) {
 	switch {
-	case cf.List:
+	case cf.ListCustomers:
 		customers.PrintCustomersTable()
-	case cf.Add != "":
-		parts := strings.SplitN(cf.Add, ":", 5)
+	case cf.AddCustomer != "":
+		parts := strings.SplitN(cf.AddCustomer, ":", 5)
 
 		if len(parts) != 5 {
 			fmt.Println("Error, invalid format for adding the customer. Please provide the whole personal information.")
@@ -51,9 +51,9 @@ func (cf *CustomerCmdFlags) Execute(customers *Customers, vehicles Vehicles) {
 		personalID, _ := strconv.Atoi(parts[4])
 
 		customers.AddCustomer(firstName, lastName, phoneNumber, email, int64(personalID))
-	case cf.Edit != "":
-		fmt.Println("cf.Edit", cf.Edit)
-		parts := strings.SplitN(cf.Edit, ":", 3)
+	case cf.EditCustomer != "":
+		fmt.Println("cf.EditCustomer", cf.EditCustomer)
+		parts := strings.SplitN(cf.EditCustomer, ":", 3)
 
 		if len(parts) != 3 {
 			fmt.Println("Error, invalid format for editting the customer. Don't forget to sepparate a phone number and email with :")
@@ -65,16 +65,16 @@ func (cf *CustomerCmdFlags) Execute(customers *Customers, vehicles Vehicles) {
 		email := parts[2]
 
 		customers.EditCustomerContacts(int64(personalID), phoneNumber, email)
-	case cf.Delete != 0:
-		personalID := cf.Delete
+	case cf.DeleteCustomer != 0:
+		personalID := cf.DeleteCustomer
 		if intLength(personalID) != 11 {
 			fmt.Println("Error, invalid format for deleting the customer. The personal ID must be exactly 11 digits long.")
 			os.Exit(1)
 		}
 
 		customers.DeleteCustomerByPersonalID(personalID)
-	case cf.AddVehicle != "":
-		parts := strings.SplitN(cf.AddVehicle, ":", 2)
+	case cf.AddVehicleToCustomer != "":
+		parts := strings.SplitN(cf.AddVehicleToCustomer, ":", 2)
 
 		if len(parts) != 2 {
 			fmt.Println("Error, invalid format for adding a vehicle to the customer. Use the following format --> personalID:plateNumber")
@@ -85,9 +85,9 @@ func (cf *CustomerCmdFlags) Execute(customers *Customers, vehicles Vehicles) {
 		plateNumber := parts[1]
 
 		customers.AddVehicleToCustomer(int64(personalID), plateNumber, vehicles)
-	case cf.DeleteVehicle != "":
-		parts := strings.SplitN(cf.DeleteVehicle, ":", 2)
-		
+	case cf.DeleteVehicleFromCustomer != "":
+		parts := strings.SplitN(cf.DeleteVehicleFromCustomer, ":", 2)
+
 		if len(parts) != 2 {
 			fmt.Println("Error, invalid format for deleting a vehicle from the customer. Use the following format --> personalID:plateNumber")
 			os.Exit(1)
